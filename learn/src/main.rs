63a529347dd64c19007faaacfa284b8d77f9bb34
Gotam Gorabh <gautamy672@gmail.com>
2022-11-17 23:11:14 +0530

    statements and expressions

--- learn/src/main.rs
+++ learn/src/main.rs
@@ -168,6 +168,29 @@ fn main() {
     
     let x = (let y = 6);  // this is wrong because let y = 6 statement does not return a value, 
     
+    
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}"); 
+    
+    /*
+    
+    This expression:
+
+{
+    let x = 3;
+    x + 1
+}
+is a block that, in this case, evaluates to 4. 
+That value gets bound to y as part of the let statement. Note that the x + 1 line doesn’t have a semicolon at the end, 
+unlike most of the lines you’ve seen so far. Expressions do not include ending semicolons. 
+If you add a semicolon to the end of an expression, you turn it into a statement, and it will then not return a value. 
+Keep this in mind as you explore function return values and expressions next
+    
+    */
 
      
 }
