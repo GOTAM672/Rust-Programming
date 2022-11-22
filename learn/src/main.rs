10ab8a9be3a1b4298098ec1949b74e6f0479835a
Gotam Gorabh <gautamy672@gmail.com>
2022-11-22 16:38:05 +0530

    learning multilayer loop

--- learn/src/main.rs
+++ learn/src/main.rs
@@ -289,6 +289,28 @@ Keep this in mind as you explore function return values and expressions next
 
     println!("The result is {result}");
     
+    // multilayer loop 
+    
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+    
     
 }
 
